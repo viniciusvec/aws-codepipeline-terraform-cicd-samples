25baf6919a8eda35d0cc89a60f254ceef115b543
viniciusvec <vinicius_vec@hotmail.com>
2024-08-01 09:49:33 +0100

    hardcoded the desired region

--- locals.tf
+++ locals.tf
@@ -6,5 +6,6 @@
 
 locals {
   account_id = data.aws_caller_identity.current.account_id
-  region     = data.aws_region.current.name
+  # region     = data.aws_region.current.name
+  region = "eu-west-2"
 }
