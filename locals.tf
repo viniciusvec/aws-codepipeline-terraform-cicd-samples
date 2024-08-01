a8241039a6f49b0494a174cf0d023e126328290c
viniciusvec <vinicius_vec@hotmail.com>
2024-08-01 11:53:21 +0100

    fixed hardcoded region

--- locals.tf
+++ locals.tf
@@ -6,6 +6,5 @@
 
 locals {
   account_id = data.aws_caller_identity.current.account_id
-  # region     = data.aws_region.current.name
-  region = "eu-west-2"
+  region     = data.aws_region.current.name
 }
