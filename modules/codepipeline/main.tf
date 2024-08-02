14e5272801d77453c2fe59ae81d7ff7652f31363
viniciusvec <vinicius_vec@hotmail.com>
2024-08-02 20:53:29 +0100

    adding destroy routines, simplify

--- modules/codepipeline/main.tf
+++ modules/codepipeline/main.tf
@@ -9,6 +9,49 @@ resource "aws_codestarconnections_connection" "github" {
   provider_type = "GitHub"
 }
 
+resource "aws_codebuild_project" "build" {
+  name         = "build"
+  service_role = var.codepipeline_role_arn
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+  environment {
+    compute_type                = "BUILD_GENERAL1_MEDIUM"
+    image                       = "aws/codebuild/standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+    image_pull_credentials_type = "CODEBUILD"
+  }
+  source {
+    type = "CODEPIPELINE"
+  }
+
+}
+
+resource "aws_codebuild_project" "destroy" {
+  name         = "codebuilddestroy"
+  service_role = var.codepipeline_role_arn
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+  environment {
+    compute_type                = "BUILD_GENERAL1_MEDIUM"
+    image                       = "aws/codebuild/standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+    image_pull_credentials_type = "CODEBUILD"
+  }
+  source {
+    buildspec = "./buildspec_destroy.yml"
+    type      = "CODEPIPELINE"
+  }
+  # source {
+  #   buildspec = "./buildspec_destroy.yml"
+  #   location  = var.s3_bucket_name
+  #   type      = "S3"
+  # }
+}
+
 resource "aws_codepipeline" "terraform_pipeline" {
 
   name     = "${var.project_name}-pipeline"
@@ -43,24 +86,73 @@ resource "aws_codepipeline" "terraform_pipeline" {
     }
   }
 
-  dynamic "stage" {
-    for_each = var.stages
+  # dynamic "stage" {
+  #   for_each = var.stages
 
-    content {
-      name = "Stage-${stage.value["name"]}"
-      action {
-        category         = stage.value["category"]
-        name             = "Action-${stage.value["name"]}"
-        owner            = stage.value["owner"]
-        provider         = stage.value["provider"]
-        input_artifacts  = [stage.value["input_artifacts"]]
-        output_artifacts = [stage.value["output_artifacts"]]
-        version          = "1"
-        run_order        = index(var.stages, stage.value) + 2
+  #   content {
+  #     name = "Stage-${stage.value["name"]}"
+  #     action {
+  #       category         = stage.value["category"]
+  #       name             = "Action-${stage.value["name"]}"
+  #       owner            = stage.value["owner"]
+  #       provider         = stage.value["provider"]
+  #       input_artifacts  = stage.value["provider"] == "CodeBuild" ? [stage.value["input_artifacts"]] : null
+  #       output_artifacts = stage.value["provider"] == "CodeBuild" ? [stage.value["output_artifacts"]] : null
+  #       version          = "1"
+  #       run_order        = index(var.stages, stage.value) + 2
 
-        configuration = {
-          ProjectName = stage.value["provider"] == "CodeBuild" ? "${var.project_name}-${stage.value["name"]}" : null
-        }
+  #       configuration = {
+  #         ProjectName = stage.value["provider"] == "CodeBuild" ? "${var.project_name}-${stage.value["name"]}" : null
+  #       }
+  #     }
+  #   }
+  # }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["SourceOutput"]
+      output_artifacts = ["BuildOutput"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.build.id
+      }
+    }
+  }
+
+  stage {
+    name = "Approve"
+
+    action {
+      name     = "DeletionApproval"
+      category = "Approval"
+      owner    = "AWS"
+      provider = "Manual"
+      version  = "1"
+    }
+  }
+
+
+  stage {
+    name = "Destroy"
+
+    action {
+      name             = "Destroy"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["BuildOutput"]
+      output_artifacts = ["DestroyOutput"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.destroy.id
       }
     }
   }
